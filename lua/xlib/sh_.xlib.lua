4a8cb3c12b4a47cdd0abc8384ef03a6cdfdfcf4b
Xavier Bergeron <xavbergeron@gmail.com>
2019-06-04 22:10:53 -0500

    Add XLIB.PostInitEntity, a hook to be executed on InitPostEntity and in subsequent Lua auto-refresh updates

--- lua/xlib/sh_.xlib.lua
+++ lua/xlib/sh_.xlib.lua
@@ -7,6 +7,20 @@ function XLIB.PostInitialize(fn)
 		hook.Add("Initialize", "XLIB.PostInitialize"..tostring(fn), fn)
 	end
 end
+
+XLIB.DidInitPostEntity = XLIB.DidInitPostEntity or false
+function XLIB.PostInitEntity(fn)
+	if XLIB.DidInitPostEntity then
+		fn()
+	else
+		hook.Add("InitPostEntity", "XLIB.PostInitialize"..tostring(fn), fn)
+	end
+end
+
+hook.Add("InitPostEntity", "XLIB.PostInitEntity", function()
+	XLIB.DidInitPostEntity = true
+end)
+
 _R = debug.getregistry()
 _P = _R.Player
 _E = _R.Entity
