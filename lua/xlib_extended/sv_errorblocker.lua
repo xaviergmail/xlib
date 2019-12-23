e424b83d65e3e8688fd2fd85f4ecc74ca4c3f66a
Xavier Bergeron <xavbergeron@gmail.com>
2019-12-23 01:30:52 -0800

    Add startup error join blocker

---
+++ lua/xlib_extended/sv_errorblocker.lua
@@ -0,0 +1,43 @@
+pcall(require, "luaerror")
+
+if not luaerror then return end
+
+luaerror.EnableRuntimeDetour(true)
+
+XLIB.StartupErrors = {}
+XLIB.StartupErrorCount = 0
+local function OnLuaError(is_runtime, rawErr, file, lineno, err, stack)
+	if not XLIB.StartupErrors[rawErr] then
+		XLIB.StartupErrorCount = XLIB.StartupErrorCount + 1
+	end
+	XLIB.StartupErrors[rawErr] = true
+end
+
+function XLIB.BlockStartup(reason)
+	OnLuaError(nil, reason)
+end
+
+hook.Add("LuaError", "XLib Error Blocker", OnLuaError)
+
+hook.Add("InitPostEntity", "XLib Error Blocker", function()
+	hook.Remove("LuaError", "XLib Error Blocker")
+end)
+
+hook.Add("CheckPassword", "XLib Error Blocker", function()
+	if XLIB.StartupErrorCount > 0 and not XLIB.StartupErrorBypass then
+		return false, ""
+	end
+end)
+
+DevCommand("xlib_clearstartuperrors", function()
+	XLIB.StartupErrorBypass = true
+	print("Bypassing startup errors")
+end)
+
+DevCommand("xlib_startuperrors", function()
+	for k, v in pairs(XLIB.StartupErrors) do
+		MsgN("-----")
+		MsgC(Color(0, 255, 255), v)
+		Msg("\n\n")
+	end
+end)
