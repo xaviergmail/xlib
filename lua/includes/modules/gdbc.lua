aceaae41d1364070724c4f0f278013bdfa07086b
Xavier Bergeron <xavbergeron@gmail.com>
2020-07-16 01:14:03 -0700

    GDBC: Added support for actual prepared statements, multi-connection pool, proper fakelag, chain-specific query logging

--- lua/includes/modules/gdbc.lua
+++ lua/includes/modules/gdbc.lua
@@ -8,10 +8,15 @@ local function IsMySQLOODB(tbl)
 end
 
 if not mysqloo then
-    require("mysqloo")
+	require("mysqloo")
 end
 
-local fakelag = CreateConVar("gdbc_fakelag", "0", FCVAR_NONE, "Sets the latency in seconds to delay MySQL callbacks by", 0, 5000)
+if (mysqloo.VERSION != "9") then
+	error("Using outdated mysqloo version")
+end
+
+local fakedelay = CreateConVar("gdbc_fakedelay", "0", FCVAR_NONE, "Sets the latency in milliseconds to delay MySQL callbacks by", 0, 30)
+local fakelag = CreateConVar("gdbc_fakelag", "0", FCVAR_NONE, "Sets the latency in milliseconds to SLEEP() by to simulate slow queries", 0, 30)
 
 local DB =
 {
@@ -41,7 +46,7 @@ if not getmetatable(DB) then setmetatable(DB, DB_mt) end
 file.Append("query_log.txt", "\n\n\n\n"..util.DateStamp().."\n")
 local concat = table.concat
 local function log(identifier, ...)
-	local args = {...}
+	local args = table.PackNil(...)
 
 	local str = ("[%s] %s\n\n"):format(identifier, concat(args, ", "))
 
@@ -49,7 +54,15 @@ local function log(identifier, ...)
 	file.Append("query_log.txt", str)
 end
 
-GDBC_LOG = false
+GDBC_LOG = GDBC_LOG
+
+if DevCommand then
+	DevCommand("gdbc_log", function()
+		GDBC_LOG = not GDBC_LOG
+		print("GDBC Log is now ", GDBC_LOG and "ACTIVE" or "inactive")
+	end)
+end
+
 local traceback = ""
 
 local Query =
@@ -110,43 +123,98 @@ end
 function Query:run(...)
 	if not self.sequence then
 		error("Query has invalid sqeuence?? Aborting.\n")
-	elseif !IsMySQLOODB(self.sequence.database) then
+	elseif not IsMySQLOODB(self.sequence.database) then
 		error("Sequence has invalid database?? Aborting.\n")
 	end
 
-	if type(self.query) == "function" then
-		self.formatargs = {...}
-		self.sql = self.query(...)
-	elseif type(self.query) == "string" then
+	local shouldLog = self.sequence.shouldLog or GDBC_LOG
+
+	local db = self.sequence.database
+
+	if isfunction(self.query) then
+		self.formatargs = table.PackNil(...)
+		self.sql = self.query(...).sql
+	elseif isstring(self.query) then
 		self.sql = self.query
-	else
+	elseif not istable(self.query) then
 		error("Wrong query type. Expected 'function' or 'string' but got "..type(self.query).."\n")
 	end
 
 	local id = os.time()
 
-	-- TODO: Switch to **actual** prepared statements
-	local sql = ""
-	local l = #self.sql
-	for i=1, l do
-		local c = self.sql[i]
-		if c == '?' then
-			local s = table.remove(self.formatargs, 1)
-			if isstring(s) then
-				s = self.sequence.database:escape(s)
+	local usePrepared = not fakelag:GetBool() and db.usePreparedStatements
+	local query
+	local sql = self.sql or ""
+	local traceback = ""
+
+	if usePrepared and self.prepared then
+		local conn = db:getLeastOccupiedDB()
+		local connid = conn.ConnectionID
+
+		query = self.prepared[connid]
+
+		if query then
+			if shouldLog then
+				local data = self.sql.."\nArgs: "..SPrintTable(self.formatargs)
+				log(id, "Executing Prepared Statement:\n"..data)
+				traceback = "Prepared Statement:\n"..data.."\nTraceback:\n"..debug.traceback()
 			end
 
-			sql = sql .. "'" .. s .. "'"
-		else
-			sql = sql .. c
+			for k, v in ipairs(self.formatargs) do
+				local t = TypeID(v)
+				if t == table.NIL then
+					query:setNull(k)
+				elseif tonumber(v) then
+					query:setNumber(k, tonumber(v))
+				elseif isbool(v) then
+					query:setBool(k, v)
+				elseif isstring(v) then
+					query:setString(k, v)
+				else
+					error("GDBC: Tried to pass invalid argument to prepared query of type "..type(v))
+				end
+			end
 		end
 	end
-	if GDBC_LOG then
-		log(id, "Executing Query: "..sql)
-		traceback = debug.traceback()
+
+	if shouldLog then
+		print("Query had", format, query, self.query)
 	end
 
-	local query = self.sequence.database:query(sql)
+	if self.format and not query then
+		-- TODO: Switch to **actual** prepared statements
+		sql = ""
+		local l = #self.sql
+		for i=1, l do
+			local c = self.sql[i]
+			if c == '?' then
+				local s = table.remove(self.formatargs, 1)
+				if s == table.NIL then
+					s = 'NULL'
+				elseif isstring(s) then
+					s = self.sequence.database:escape(s)
+				end
+
+				sql = sql .. "'" .. s .. "'"
+			else
+				sql = sql .. c
+			end
+		end
+	end
+
+	if not query then
+		if fakelag:GetBool() then
+			sql = sql .. "; DO SLEEP("..(fakelag:GetInt()/1000)..");"
+		end
+
+		if shouldLog then
+			log(id, "Executing Query:\n"..sql)
+			traceback = debug.traceback()
+		end
+
+
+		query = self.sequence.database:query(sql)
+	end
 
 	function query.onAborted(q)
 	end
@@ -161,8 +229,8 @@ function Query:run(...)
 
 	 	local args = {err=err, sql=sql, traceback=traceback}
  		log(id, "FAILED:\n"..SPrintTable(args).."\n")
- 		if fakelag:GetBool() then
-	 		timer.Simple(fakelag:GetInt()/1000, function() self:onFailure(err, sql, traceback) end)
+ 		if fakedelay:GetBool() then
+	 		timer.Simple(fakedelay:GetInt()/1000, function() self:onFailure(err, sql, traceback) end)
  		else
 	 		self:onFailure(err, sql, traceback)
 	 	end
@@ -177,8 +245,8 @@ function Query:run(...)
 			end
 		end
 
-	 	if fakelag:GetBool() then
-		 	timer.Simple(fakelag:GetInt()/1000, function() self:onSuccess(data, q:lastInsert()) end)
+	 	if fakedelay:GetBool() then
+		 	timer.Simple(fakedelay:GetInt()/1000, function() self:onSuccess(data, q:lastInsert()) end)
 	 	else
 		 	self:onSuccess(data, q:lastInsert())
 		 end
@@ -195,14 +263,24 @@ Query.__index = Query
 setmetatable(Query,
 {
 	__call = function(_, sequence, query, all, format, ...)
+		local formatargs = table.PackNil(...)
+		local prepared
+
+		if istable(query) then
+			prepared = query.prepared
+			formatargs = query.formatargs
+			query = query.sql
+		end
+
 		return setmetatable(
 			{
 				sequence = sequence,
 				query = query,
 				all = all,
 				format = format,
-				formatargs = {...},
+				formatargs = formatargs,
 				data = sequence.data,
+				prepared = prepared,
 			}, Query)
 	end
 })
@@ -211,7 +289,7 @@ local QuerySequence = {}
 function QuerySequence:___stringwrap(func)
 	return function(_, str, ...)
 		if type(str) == "string" then
-			local args = {...}
+			local args = table.PackNil(...)
 
 			return setmetatable({},
 			{
@@ -220,7 +298,7 @@ function QuerySequence:___stringwrap(func)
 					return self
 				end,
 				__index = function(t, k)
-					func(self, nil, str, unpack(args))
+					func(self, nil, str, table.UnpackNil(args))
 
 					local v = self[k]
 
@@ -308,7 +386,7 @@ function QuerySequence:_exec_sw(str, data)
 		args = self.data.args
 	end
 
-	self:proceed(str, unpack(args))
+	self:proceed(str, table.UnpackNil(args))
 end
 
 function QuerySequence:_get_(k)
@@ -321,6 +399,11 @@ function QuerySequence:_set_(k, v)
 	if self.data then
 		self.data[k] = v
 	end
+end
+
+function QuerySequence:_log_()
+	self.shouldLog = true
+	return self
 end
 
 function QuerySequence.___splitargs(str)
@@ -444,7 +527,7 @@ local function schema(name)
 		table_insert(tbl, make_config_table())
 		table_insert(tbl, make_config_migration())
 
-		local _schema = {name=name, migrations={}}
+		local _schema = {name=name, migrations={}, tables={}}
 		for k, v in pairs(tbl) do
 			if IsMySQLOODB(v) then
 				_schema.database = v
@@ -453,6 +536,7 @@ local function schema(name)
 					_schema.migrations[v.id] = v.query
 				else
 					_schema[v[1]] = v[2]
+					_schema.tables[v[1]] = v[2]
 				end
 			else
 				Error("GDBC: Could not connect to database!")
@@ -460,49 +544,69 @@ local function schema(name)
 		end
 		setmetatable(_schema, schema_mt)
 		DB.__schemas[name] = _schema
+
+		local db = _schema.database
+
+		function db:prepareStatements(conn)
+			for tblName, tbl in pairs(_schema.tables) do
+				for name, sql in pairs(tbl.__queries) do
+					tbl.__prepared[name][conn.ConnectionID] = conn:prepare(sql)
+				end
+			end
+		end
+
+		db:connect()
 	end
 end
 
 local function connect(i)
 	if !mysqloo then return end
-	local db = mysqloo.connect(i.host, i.user, i.pass, i.database, i.port)
 
+	local threads = i.threads or 1
+
+	local db
+	if threads > 1 then
+		db = mysqloo.CreateConnectionPool2(threads, i.host, i.user, i.pass, i.database, i.port)
+	else
+		db = mysqloo.connect(i.host, i.user, i.pass, i.database, i.port)
+		db.ConnectionID = 1
+		db.getLeastOccupiedDB = function() return db end
+	end
+
+	db.usePreparedStatements = i.usePreparedStatements
 
 	function db:onConnected(err)
-		print(i.database..": Connected!")
+		local pid = self.ConnectionID or 1
+
+		local str = "!"
+		if db.usePreparedStatements	then
+			db:prepareStatements(self)
+			str = " and using prepared statements!"
+		end
+
+		print(i.database.."["..pid.."]: Connected"..str)
 	end
 
 	function db:onConnectionFailed(err)
-		Error("Database '"..i.database.."' - connection failed with username ".. i.user .."\n")
+		local pid = self.ConnectionID or 1
+		Error("Database '"..i.database.."'["..pid.."] - connection failed with username ".. i.user .."\n")
 	end
 
 	db:setAutoReconnect(true)
-    db:connect()
 
 	return db
 end
 
-local function prepare(query, ...)
-	local format = {...}
-	local count = 0
-	for _ in string.gfind(query, "?") do
-		count = count + 1
-	end
-
-	if #format ~= count then
-		error(string.format("DB: Prepared statement called with improper argument count: expected %d but got %d instead.\n %s", count, #format, debug.traceback()))
-	end
-
-	return query, unpack(format)
-end
-
 local function escape(str)
+	if str == table.NIL then return 'NULL' end
 	if isnumber(str) then return str end
 	return str:gsub("'", "\\'")
 end
 
 local table_funcs = {}
 
+-- XXX: Misnomer!! This doesn't _prepare_ a statement but rather formats and escapes
+-- a query using prepared statement syntax `?` and returns the given SQL string.
 function table_funcs:prepare(query, ...)
 	if self.__queries[query] then
 		local query = self.__queries[query]
@@ -511,7 +615,7 @@ function table_funcs:prepare(query, ...)
 		local len = string.len(query)
 
 		local idx = 1
-		local formatargs = {...}
+		local formatargs = table.PackNil(...)
 
 		for i = 1, string.len(query) do
 			local c = string.sub(query, i, i)
@@ -538,7 +642,19 @@ local table_mt =
 	__index = function(t, k)
 		if t.__queries and t.__queries[k] then
 			return function(...)
-				return prepare(t.__queries[k], ...)
+				local sql = t.__queries[k]
+
+				local argcount = select('#', ...)
+				local count = 0
+				for _ in string.gfind(sql, "?") do
+					count = count + 1
+				end
+
+				if argcount ~= count then
+					error(string.format("DB: Prepared statement called with improper argument count: expected %d but got %d instead.\n %s", count, argcount, debug.traceback()))
+				end
+
+				return {sql=sql, formatargs=table.PackNil(...), prepared=t.__prepared[k]}
 			end
 		end
 
@@ -552,15 +668,18 @@ local table_mt =
 
 local function table(name)
 	return function (tbl)
-		for k, v in pairs(tbl) do
-			v = v:gsub("__T__", name):gsub("%s+", " ")
-
-			if v:sub(v:len()) != ";" then
-				v = v..";"
-			end
-			tbl[k] = v
-		end
 		_table = {}
+		_table.__prepared = {}
+		for queryName, sql in pairs(tbl) do
+			_table.__prepared[queryName] = {}
+
+			sql = sql:gsub("__T__", name):gsub("%s+", " ")
+
+			if sql:Trim():sub(sql:len()) != ";" then
+				sql = sql..";"
+			end
+			tbl[queryName] = sql
+		end
 		_table.__queries = tbl
 		setmetatable(_table, table_mt)
 		return {name, _table}
@@ -772,6 +891,106 @@ function perform_migrations(db)
 	end
 
 	check_migrations(0)
+end
+
+
+
+local spreadAttributes = { onConnected=true, onConnectionFailed=true }
+local pool = {}
+local poolMT = {
+	MetaName = MySQLOO_MetaName,
+	__index = function(t, k)
+		if spreadAttributes[k] then
+			return self._Connections[1][k]
+		end
+		return rawget(pool, k)
+	end,
+	__newindex = function(self, k, v)
+		if spreadAttributes[k] then
+			for _, db in ipairs(self._Connections) do
+				db[k] = v
+			end
+		else
+			rawset(self, k, v)
+		end
+	end
+}
+
+function mysqloo.CreateConnectionPool2(conCount, ...)
+	if (conCount < 1) then
+		conCount = 1
+	end
+
+	local newPool = setmetatable({}, poolMT)
+	newPool._Connections = {}
+
+	for i = 1, conCount do
+		local db = mysqloo.connect(...)
+		db.ConnectionID = i
+		table_insert(newPool._Connections, db)
+	end
+
+	return newPool
+end
+
+function pool:queueSize()
+	local count = 0
+	for _, v in pairs(self._Connections) do
+		count = count + v:queueSize()
+	end
+	return count
+end
+
+function pool:abortAllQueries()
+	for _, v in pairs(self._Connections) do
+		v:abortAllQueries()
+	end
+end
+
+function pool:getLeastOccupiedDB()
+	local lowest = nil
+	local lowestCount = 0
+	for _, db in ipairs(self._Connections) do
+		local queueSize = db:queueSize()
+		if (not lowest or queueSize < lowestCount) then
+			lowest = db
+			lowestCount = queueSize
+		end
+	end
+
+	if not lowest then
+		error("Failed to find available database from connection pool")
+	end
+
+	return lowest
+end
+
+local overrideFunctions = {"escape", "query", "prepare", "createTransaction", "status", "serverVersion", "hostInfo", "serverInfo", "ping"}
+for _, name in ipairs(overrideFunctions) do
+	pool[name] = function(pool, ...)
+		local db = pool:getLeastOccupiedDB()
+		return db[name](db, ...)
+	end
+end
+
+local spreadFunctions = {"setAutoReconnect", "setMultiStatements", "setCachePreparedStatements", "wait", "setCharacterSet", "connect", "disconnect"}
+for _, name in ipairs(spreadFunctions) do
+	pool[name] = function(pool, ...)
+		for k, db in ipairs(pool._Connections) do
+			db[name](db, ...)
+		end
+	end
+end
+
+-- Allows us to get the return values from these functions passed through a callback
+local cbSpreadFunctions = {"setCharacterSets"}
+for _, name in ipairs(cbSpreadFunctions) do
+	local rets = {}
+	pool[name] = function(pool, cb, ...)
+		for k, db in ipairs(pool._Connections) do
+			cb(k, db[name](db, ...))
+		end
+	end
 end
 
 print("GDBC Wrapper loaded")
