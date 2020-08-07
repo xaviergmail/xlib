b2776da1274c5164b041b8f532db38b9acb05a6e
Xavier Bergeron <xavbergeron@gmail.com>
2020-08-06 22:01:46 -0700

    Update sh_traces to not break GLDoc

--- lua/xlib/sh_traces.lua
+++ lua/xlib/sh_traces.lua
@@ -1,22 +1,22 @@
 -- Credits: code_gs AKA github.com/Kefta
 -- https://github.com/Facepunch/garrysmod/pull/847/
 
---[[---------------------------------------------------------
-	Name:	TraceEntityOBB
-	Params:	<table tracedata> <ent entity> <bool quick> <bool verbose>
-	Returns: <table result> <table verboseresults>
-	Desc:	Traces using an entity's bounding box offset along a line.
-	"Quick" parameter skips tracing through every corner and only traces between the start corner and end corner.
-	"Verbose" parameter returns every trace performed in a table
-	WARNING: This traces lines between an ent's bounding box corners. Small entities may "slip through".
-	TraceData structure:
-		Vector mins: Offset mins (lower back left corner). Optional.
-		Vector maxs: Offset maxs (upper front right corner). Optional.
-		Vector start: Traces from this position, if you /really/ want to. Optional.
-		Vector endpos: Traces from the entity's position (or the starting position) to this position. Optional.
-		Entity filter: Things the trace should not hit. Can also be a table of entities or a function with one argument.
-		boolean ignoreworld: Should the trace ignore world or not
-		table output: If set, the trace result will be written to the supplied table instead of returning a new table
+--[[ --------------------------------------------------------
+--	Name:	TraceEntityOBB
+--	Params:	<table tracedata> <ent entity> <bool quick> <bool verbose>
+--	Returns: <table result> <table verboseresults>
+--	Desc:	Traces using an entity's bounding box offset along a line.
+--	"Quick" parameter skips tracing through every corner and only traces between the start corner and end corner.
+--	"Verbose" parameter returns every trace performed in a table
+--	WARNING: This traces lines between an ent's bounding box corners. Small entities may "slip through".
+--	TraceData structure:
+--		Vector mins: Offset mins (lower back left corner). Optional.
+--		Vector maxs: Offset maxs (upper front right corner). Optional.
+--		Vector start: Traces from this position, if you /really/ want to. Optional.
+--		Vector endpos: Traces from the entity's position (or the starting position) to this position. Optional.
+--		Entity filter: Things the trace should not hit. Can also be a table of entities or a function with one argument.
+--		boolean ignoreworld: Should the trace ignore world or not
+--		table output: If set, the trace result will be written to the supplied table instead of returning a new table
 -----------------------------------------------------------]]
 function util.TraceEntityOBB(tracedata, ent, quick, verbose)
 	local mins, maxs = ent:GetCollisionBounds()
@@ -90,23 +90,23 @@ function util.TraceEntityOBB(tracedata, ent, quick, verbose)
 	return tr
 end
 
- --[[---------------------------------------------------------
-	Name:	TraceOBB
-	Params:	<table tracedata> <bool quick> <bool verbose>
-	Returns: <table result> <table verboseresults>
-	Desc:	Traces using a bounding box offset along a line.
-	"Quick" parameter skips tracing through every corner and only traces between the start corner and end corner.
-	"Verbose" parameter returns every trace performed in a table
-	WARNING: This traces lines between bounding box corners. Small entities may "slip through".
-	TraceData structure:
-		Vector mins: OBB mins (lower back left corner).
-		Vector maxs: OBB maxs (upper front right corner).
-		Angle angles: Angles of the OBB.
-		Vector start: The start position of the trace.
-		Vector endpos: The end position of the trace.
-		Entity filter: Things the trace should not hit. Can also be a table of entities or a function with one argument.
-		boolean ignoreworld: Should the trace ignore world or not
-		table output: If set, the trace result will be written to the supplied table instead of returning a new table
+--[[ --------------------------------------------------------
+--	Name:	TraceOBB
+--	Params:	<table tracedata> <bool quick> <bool verbose>
+--	Returns: <table result> <table verboseresults>
+--	Desc:	Traces using a bounding box offset along a line.
+--	"Quick" parameter skips tracing through every corner and only traces between the start corner and end corner.
+--	"Verbose" parameter returns every trace performed in a table
+--	WARNING: This traces lines between bounding box corners. Small entities may "slip through".
+--	TraceData structure:
+--		Vector mins: OBB mins (lower back left corner).
+--		Vector maxs: OBB maxs (upper front right corner).
+--		Angle angles: Angles of the OBB.
+--		Vector start: The start position of the trace.
+--		Vector endpos: The end position of the trace.
+--		Entity filter: Things the trace should not hit. Can also be a table of entities or a function with one argument.
+--		boolean ignoreworld: Should the trace ignore world or not
+--		table output: If set, the trace result will be written to the supplied table instead of returning a new table
 -----------------------------------------------------------]]
 function util.TraceOBB(tracedata, quick, verbose)
 	local mins, maxs = tracedata.mins, tracedata.maxs
