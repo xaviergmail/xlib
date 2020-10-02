3c1588777b8e99c779b9a57aef4e6e38a859a2e7
Xavier Bergeron <xavbergeron@gmail.com>
2020-10-02 01:15:11 -0700

    Add XLIB.Compare and XLIB.CompareString to test suite assertions

--- lua/xlib_extended/sh_testing.lua
+++ lua/xlib_extended/sh_testing.lua
@@ -35,17 +35,131 @@ XLIB.PostInitEntity(function()
 	end)
 end)
 
+
+local function concat(t)
+	local s = ""
+	for _, v in ipairs(t) do
+		s = s .. "][" .. tostring(v)
+	end
+
+	return s:Trim()
+end
+
+function XLIB.CompareString(cmpa, cmpb)
+	if cmpa == cmpb then return true end
+
+	local reason = ""
+	local stralen, strblen = cmpa:len(), cmpb:len()
+
+	local len = math.min(stralen, strblen)
+
+	if stralen != strblen then
+		reason = ("String lengths differed A: %d B: %d\n"):format(stralen, strblen)
+	end
+
+	for i=1, len do
+		if cmpa[i] != cmpb[i] then
+			local inspectLen = 30
+			local back = math.min(i, inspectLen)
+			reason = reason ..  "A: "..cmpa:sub((i-back + 1), i+inspectLen).."\n"..
+								(" "):rep(back+2).."|\n".. 
+								"B: "..cmpb:sub((i-back + 1), i+inspectLen)
+			break
+		end
+	end
+
+	return false, reason
+end
+
+local stack
+local function ret(val, va, vb, reason)
+	if not val and stack and not reason then
+		local path = ""
+		while stack:Top() do
+			path = stack:Pop().."]["..path
+		end
+		path = "["..path:sub(1, path:len()-1)
+
+		reason = "Differed on index "..path..":".."\n"..
+				 "A"..path..": "..tostring(va).."\n"..
+				 "B"..path..": "..tostring(vb).."\n"
+		stack = nil
+	end
+
+	return val, reason
+end
+function XLIB.Compare(a, b, nometa, visited)
+	visited = visited or {}
+	if not stack then
+		stack = util.Stack()
+	end
+
+	if istable(a) then
+		if istable(b) and a == b then return ret(true) end
+		if visited[a] then return ret(true) end
+		visited[a] = true
+	end
+
+	if istable(b) then
+		if visited[b] then return ret(true) end
+		visited[b] = true
+	end
+
+	if isstring(a) and isstring(b) and a != b then
+		local succ, reason = XLIB.CompareString(a, b)
+		if not succ then
+			stack = nil
+			return ret(false, a, b, "Strings differed:\n"..reason)
+		else
+			return true
+		end
+	end
+
+
+	-- nil and 'no value' are different and won't pass type()
+	if a == nil or b == nil then return ret(a == b, a, b) end
+	if type(a) != type(b) then return ret(false, a, b) end
+	if not istable(a) and not istable(b) then return ret(a == b, a, b) end
+	if isfunction((getmetatable(a) or {}).__eq) then return ret(a == b, a, b) end
+
+	local get = nometa and f.index or rawget
+
+	for k, va in pairs(a) do
+		local vb = get(b, k)
+		stack:Push(k)
+		local succ, reason = XLIB.Compare(va, vb, nometa, visited)
+		if not succ then return ret(false, va, vb, reason) end
+		stack:Pop()
+	end
+
+
+	for k, vb in pairs(b) do
+		local va = get(a, k)
+		stack:Push(k)
+		local succ, reason = XLIB.Compare(va, vb, nometa, visited)
+		if not succ then return ret(false, va, vb, reason) end
+		stack:Pop()
+	end
+
+	return true
+end
+
 function XLIB.Test(name, test)
 	local testData = {id = name, test=test, status=XLIB.Tests.QUEUED}
 
-	local function callback(reason, success, strb)
+	local function callback(reason, cmpa, cmpb)
 		-- Be careful, as soon as the first assertion callback is called,
 		-- the entire test is considered "complete" in the eyes of
 		-- the player connection denial gate.
-		local stra
-		if isstring(success) then
-			stra = success
-			success = stra == strb
+
+
+		local success, err = false
+		if isbool(cmpa) and cmpb == nil then
+			success = cmpa
+		elseif isstring(cmpa) and isstring(cmpb) then
+			success, err = XLIB.CompareString(cmpa, cmpb)
+		else
+			success, err = XLIB.Compare(cmpa, cmpb)
 		end
 
 		local fmt = ('[ %s ] -> [ %s ]'):format(name, reason)
@@ -57,6 +171,7 @@ function XLIB.Test(name, test)
 			XLIB.Tests.Succeeded[fmt] = testData
 
 			Log("Passed - ", fmt)
+			if err then Log(err) end
 		else
 			testData.status = XLIB.Tests.FAILED
 
@@ -65,29 +180,10 @@ function XLIB.Test(name, test)
 
 			testData.failReason = reason
 			Err("Failed - ", fmt)
+			if err then Err(err) end
+
 			if sentry then
 				sentry.ReportError("Server startup blocked by failed test " .. name .. ": " .. reason, {}, { err=err })
-			end
-
-			if stra then
-				local stralen, strblen = stra:len(), strb:len()
-
-				local len = math.min(stralen, strblen)
-
-				if stralen != strblen then
-					Err(("String lengths differed A: %d B: %d"):format(stralen, strblen))
-				end
-
-				for i=1, len do
-					if stra[i] != strb[i] then
-						local inspectLen = 30
-						local back = math.min(i, inspectLen)
-						Err("A:", stra:sub((i-back + 1), i+inspectLen))
-						Err((" "):rep(back+2).."|")
-						Err("B:", strb:sub((i-back + 1), i+inspectLen))
-						break
-					end
-				end
 			end
 		end
 	end
