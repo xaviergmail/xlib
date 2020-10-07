17412ced1cf03530e10d2c7e8b717f5a2d6e4d31
Xavier Bergeron <xavbergeron@gmail.com>
2020-10-06 23:25:51 -0700

    Add testserver.toggle devcommand

--- lua/xlib/xlib_extended.lua
+++ lua/xlib/xlib_extended.lua
@@ -1,8 +1,7 @@
-XLIB.Extended = true
-
 if SERVER then
 	if XLIB.Extended then
 		AddCSLuaFile()
+		SetGlobalBool("development_mode", CREDENTIALS.CHECK.development_mode == 1)
 	else
 		return
 	end
@@ -14,6 +13,22 @@ local devports = {
 	["13337"] = true,
 	["13338"] = true,
 }
+
 function IsTestServer()
 	return GetGlobalBool("development_mode") and devports[game.GetIPAddress():Split(":")[2]]
 end
+
+DevCommand("testserver.toggle", function(ply)
+	if not devports[game.GetIPAddress():Split(":")[2]] or CREDENTIALS.CHECK.production then
+		ply:ChatPrint("You cannot change the test server status on a production server! (Check hostport)")
+		return
+	end
+
+	if IsValid(ply) then
+		ply:ChatPrint("You can only run this from console")
+		return
+	end
+
+	SetGlobalBool("development_mode", !GetGlobalBool("development_mode", false))
+	print("Dev mode is ", GetGlobalBool("development_mode") and "Enabled" or "Disabled")
+end)
